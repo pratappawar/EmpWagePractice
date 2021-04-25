214be2060d29ec28fd3649c91194f368363744a0
Pratap Pawar <pratappawar1997@gmail.com>
2021-04-25 19:01:26 +0530

    ADDED: USE CASE ADDED TO EMPWAGE

--- EmpWage.java
+++ EmpWage.java
@@ -3,6 +3,8 @@
 	public static void main(String[] args){
 		System.out.println("Hello World");
 		System.out.println("Employee wage computation is added:");
+		System.out.println("Employee wage computation USE CASE 2 is added:");
+
 	}
 
     }
